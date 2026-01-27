2820e71755eb112858ab51d3ac87ceb19dc4219d
eutentiofficial-art <eutentiofficial@gmail.com>
2026-01-27 14:32:52 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import TestSupabase from './components/TestSupabase';
+import ComparisonFunnel from './components/ComparisonFunnel';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-slate-50 flex items-center justify-center py-12">
-      <TestSupabase />
+      <ComparisonFunnel />
     </div>
   );
 };
